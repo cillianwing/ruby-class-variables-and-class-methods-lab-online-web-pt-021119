c4c3f0caae167b30f32f2078003c90ba699795c1
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-12 17:06:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   
   attr_accessor :name, :artist, :genre 
   
-  def initialize
+  def initialize(name, artist, genre)
+    @song_name = name 
+    @artist_name = artist 
+    @genre = genre 
+  end
   
 end
